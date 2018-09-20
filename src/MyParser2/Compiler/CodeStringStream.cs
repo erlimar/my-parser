ea19dd3c767f1ed93e7ddef940cdd99d2db71389
Erlimar Silva Campos <erlimar@gmail.com>
2018-09-20 19:21:26 -0300

    Implementa CodeStringStream

--- src/MyParser2/Compiler/CodeStringStream.cs
+++ src/MyParser2/Compiler/CodeStringStream.cs
@@ -1,15 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace MyParser2.Compiler
 {
     public class CodeStringStream : ObjectStream<char>
     {
-        private readonly string _code;
+        private readonly Stream _stream;
 
         public CodeStringStream(string code)
         {
-            _code = code
-                ?? throw new ArgumentNullException(nameof(code));
+            if (code == null)
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            _stream = new MemoryStream(Encoding.ASCII.GetBytes(code));
+        }
+
+        public override bool EndOfStream()
+        {
+            if (_stream.Position < _stream.Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void SetPosition(long position)
+        {
+            _stream.Position = position;
+        }
+
+        public override long GetPosition()
+        {
+            return _stream.Position;
+        }
+
+        public override char Next()
+        {
+            return (char)_stream.ReadByte();
         }
     }
 }
