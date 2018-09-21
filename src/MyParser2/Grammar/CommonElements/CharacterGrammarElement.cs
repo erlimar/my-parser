ce5c8c2ac0b5fac67081b7f16148a9f4808b6032
Erlimar Silva Campos <erlimar@gmail.com>
2018-09-21 12:28:52 -0300

    Adiciona elemento comum CharacterGrammarElement

---
+++ src/MyParser2/Grammar/CommonElements/CharacterGrammarElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using MyParser2.Lexer;
+
+namespace MyParser2.Grammar.CommonElements
+{
+    public class CharacterGrammarElement : MyGrammarElement
+    {
+        private readonly Char _character;
+
+        public CharacterGrammarElement(Char character)
+        {
+            _character = character;
+        }
+
+        public override MyToken[] Eval(ObjectStream<Char> input, MyScannerDiscardDelegate<char> discarder)
+        {
+            Ensure(input, discarder);
+
+            // Ignora código descartável inicialmente
+            input.Discard(discarder);
+
+            if (input.EndOfStream())
+            {
+                return null;
+            }
+
+            var initialPos = input.GetPosition();
+            char character = input.Next();
+
+            if (character != _character)
+            {
+                input.SetPosition(initialPos);
+
+                return null;
+            }
+
+            var token = new MyToken(
+                MakeTokenClass(),
+                initialPos,
+                input.GetPosition(),
+                character
+            );
+
+            return new MyToken[]
+            {
+                token
+            };
+        }
+    }
+}
